e65bf86242a8c15023a91e79b8651fee23ed26ff
DarshanB1873 <131136322+DarshanB1873@users.noreply.github.com>
2023-04-18 14:42:20 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Darshan
+#
+# Created:     18-04-2023
+# Copyright:   (c) Darshan 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+fahrenhiet = float(input("Enter the temperature in degree in farenhiet"))
+celcius=(fahrenhiet-32)*5/9
+print("the",fahrenhiet," fahrenhiet degree is equal to",celcius,"celcius")
